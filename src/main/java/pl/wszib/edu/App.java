54a9a55a4e57bad312d7744186cab72c3230b372
Szymon <sfyderek@gmail.com>
2022-12-03 13:47:53 +0100

    Initial commit

---
+++ src/main/java/pl/wszib/edu/App.java
@@ -0,0 +1,13 @@
+package pl.wszib.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
